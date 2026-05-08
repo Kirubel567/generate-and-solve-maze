eb33ff4ddd4c8c080c5aaee48158861a69e4c8f6
Kirubel <kirubel.wubet1996@gmail.com>
2026-05-08 12:15:15 +0300

    added solver and genearator variables

--- maze.py
+++ maze.py
@@ -16,3 +16,19 @@ northWall = [[1 for _ in range(C + 1)] for _ in range(R + 1)]
 # Initialize the eastWall array. It is of size (R+1) x (C+1).
 # Index 0 is the phantom column, making up the left edge.
 eastWall = [[1 for _ in range(C + 1)] for _ in range(R + 1)]
+
+current_r = 1 
+current_c = 1 
+stack = [] 
+generation_complete = False 
+
+
+start_cell = None 
+end_cell = None 
+solver_stack = [] 
+solver_current = None 
+
+# solver_state: a 2D array tracking each cell's status in the solver:
+solver_state = [[0 for _ in range(C + 1)] for _ in range(R + 1)]
+solver_complete = False 
+solver_started = False 
