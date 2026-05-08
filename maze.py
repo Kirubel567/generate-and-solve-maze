d0fbc430355606c5a19933c0e86706ac14661fe3
Kirubel <kirubel.wubet1996@gmail.com>
2026-05-08 12:19:52 +0300

    implemented reset_maze and reset_solver for interactive experience

--- maze.py
+++ maze.py
@@ -32,3 +32,39 @@ solver_current = None
 solver_state = [[0 for _ in range(C + 1)] for _ in range(R + 1)]
 solver_complete = False 
 solver_started = False 
+
+
+def reset_solver():
+    global solver_stack, solver_current, solver_state, solver_complete, solver_started
+
+    solver_state = [[0 for _ in range(C + 1)] for _ in range(R + 1)]
+    solver_stack = []
+    solver_complete = False 
+    solver_started = False 
+    
+    if start_cell: 
+        solver_current = start_cell 
+        solver_state[start_cell[0]][start_cell[1]] = 1 
+        print("Solver cleared! Press 'S' to start solving.") 
+
+def reset_maze():
+    global northWall, eastWall 
+    global current_r, current_c, stack, generation_complete 
+    global start_cell, end_cell 
+    
+    #re-initialize all the walls to one
+    northWall = [[1 for _ in range(C + 1)] for _ in range(R + 1)]
+    eastWall = [[1 for _ in range(C + 1)] for _ in range(R + 1)]
+    
+    current_r = 1 
+    current_c = 1 
+    stack = [] #clear the generation stack
+    generation_complete = False 
+    
+    start_cell = None 
+    end_cell = None 
+    
+    reset_solver() # Call reset_solver to clear all solver variables too
+    print("\nMaze completely reset! Generating a new maze") 
+
+    
